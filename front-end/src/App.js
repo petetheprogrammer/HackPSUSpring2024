e51d859c87ec2bf8e4f856f44de5829d3a72fcad
Kelly <tianqingkellykou@gmail.com>
2024-03-16 16:16:55 -0400

    ran react app boilerplate

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        What is the best college for you, international student?
+      </header>
+    </div>
+  );
+}
+
+export default App;
